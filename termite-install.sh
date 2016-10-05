17e10509114c3c15dd86c238d4f003fe47bbfeaa
z <z@debian.z>
2016-10-05 16:43:17 +0200

    Dowloads termite and libvte from github.

--- termite-install.sh
+++ termite-install.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 
+git clone --recursive https://github.com/thestinger/termite.git
+git clone https://github.com/thestinger/vte-ng.git
 sudo apt-get install g++ libgtk-3-dev gtk-doc-tools gnutls-bin valac
 sudo apt-get install libglib3.0-cil-dev libgnutls28-dev libgirepository1.0-dev
 sudo apt-get install libxml2-utils gperf
 cd vte-ng && ./autogen.sh && make && sudo make install
 cd ../termite && make && sudo make install
-sudo desktop-file-install termite.desktop
 sudo cp /usr/local/lib/libvte-2.91.a /usr/local/lib/libvte-2.91.la \
 /usr/local/lib/libvte-2.91.so /usr/local/lib/libvte-2.91.so.0 \
 /usr/local/lib/libvte-2.91.so.0.4400.1 /usr/lib
