677ebfb6da770552a92070be805cf59d8a603f6b
João Fernandes <joaovitorfernandes2@gmail.com>
2016-11-02 23:37:37 -0200

    Change apt-get for apt

--- termite-install.sh
+++ termite-install.sh
@@ -2,9 +2,9 @@
 
 git clone --recursive https://github.com/thestinger/termite.git
 git clone https://github.com/thestinger/vte-ng.git
-sudo apt-get install g++ libgtk-3-dev gtk-doc-tools gnutls-bin valac
-sudo apt-get install libglib3.0-cil-dev libgnutls28-dev libgirepository1.0-dev
-sudo apt-get install libxml2-utils gperf
+sudo apt install g++ libgtk-3-dev gtk-doc-tools gnutls-bin valac
+sudo apt install libglib3.0-cil-dev libgnutls28-dev libgirepository1.0-dev
+sudo apt install libxml2-utils gperf
 cd vte-ng && ./autogen.sh && make && sudo make install
 cd ../termite && make && sudo make install
 sudo desktop-file-install termite.desktop
