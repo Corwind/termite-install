c95c416f6833a29d4782db6d564a80bddde0cc19
Kamil <kamil@leno.aquilitia>
2017-02-04 11:44:25 +0100

    Updates vte-ng installation proccess.

--- termite-install.sh
+++ termite-install.sh
@@ -2,14 +2,24 @@
 
 git clone --recursive https://github.com/thestinger/termite.git
 git clone https://github.com/thestinger/vte-ng.git
-sudo apt-get install g++ libgtk-3-dev gtk-doc-tools gnutls-bin valac
-sudo apt-get install libglib3.0-cil-dev libgnutls28-dev libgirepository1.0-dev
-sudo apt-get install libxml2-utils gperf
+sudo apt-get install -y \
+	g++ \
+	libgtk-3-dev \
+	gtk-doc-tools \
+	gnutls-bin \
+	valac \
+	intltool \
+	libpcre2-dev \
+	libglib3.0-cil-dev \
+	libgnutls28-dev \
+	libgirepository1.0-dev \
+	libxml2-utils \
+	gperf
+
+echo export LIBRARY_PATH="/usr/include/gtk-3.0:$LIBRARY_PATH"
 cd vte-ng && ./autogen.sh && make && sudo make install
 cd ../termite && make && sudo make install
-sudo cp /usr/local/lib/libvte-2.91.a /usr/local/lib/libvte-2.91.la \
-/usr/local/lib/libvte-2.91.so /usr/local/lib/libvte-2.91.so.0 \
-/usr/local/lib/libvte-2.91.so.0.4400.1 /usr/lib
+sudo ldconfig
 sudo mkdir -p /lib/terminfo/x; sudo ln -s \
 /usr/local/share/terminfo/x/xterm-termite \
 /lib/terminfo/x/xterm-termite
